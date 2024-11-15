ec96b87acaf6da2cdbec6f66af6bf15833ad96d0
godenji <admin@ushr.com>
2024-11-14 21:48:02 -0800

    streamline network connection detection handling

--- src/app/util/network.ts
+++ src/app/util/network.ts
@@ -1,47 +1,42 @@
 import { Injectable } from '@angular/core'
 import { Platform } from '@ionic/angular'
 import { Network } from '@capacitor/network'
-import { BehaviorSubject } from 'rxjs'
+import { BehaviorSubject, Observable } from 'rxjs'
 
 @Injectable({ providedIn: 'root' })
 export class NetworkService {
   isNative: boolean
 
   private _connected = false
-  private _isOfflineMode = false
-  private offlineMode$ = new BehaviorSubject<boolean>(false)
+  private _connected$ = new BehaviorSubject<boolean>(null)
 
-  constructor(private platform: Platform) {
-    this.isNative = this.platform.is('hybrid')
-
-    this.initConnection()
-    this.addConnectionListener()
-    this.offlineMode$.subscribe(x => (this._isOfflineMode = x))
-  }
-
-  private initConnection() {
-    Network.getStatus().then(x => (this._connected = x.connected))
-  }
-
-  private addConnectionListener() {
-    Network.addListener('networkStatusChange', x => {
-      console.log('network status changed:', x)
-      this._connected = x.connected
-    })
+  private set connected(x: boolean) {
+    this._connected = x
+    this._connected$.next(x)
   }
 
   get connected(): boolean {
     return this._connected
   }
 
-  get isOfflineMode(): boolean {
-    return this._isOfflineMode
+  get connected$(): Observable<boolean> {
+    return this._connected$.asObservable()
   }
 
-  /**
-   * opt-in/out of offline mode
-   */
-  setOffline(x: { isOffline: boolean }) {
-    this.offlineMode$.next(x.isOffline)
+  constructor(private platform: Platform) {
+    this.isNative = this.platform.is('hybrid')
+    this.initConnection()
+    this.addConnectionListener()
+  }
+
+  private initConnection() {
+    Network.getStatus().then(x => this.connected = x.connected)
+  }
+
+  private addConnectionListener() {
+    Network.addListener('networkStatusChange', x => {
+      console.log('network status changed:', x)
+      this.connected = x.connected
+    })
   }
 }
